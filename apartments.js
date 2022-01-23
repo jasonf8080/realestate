55bbe855528122c19c57c958683ee3e378395ddd
jasonf8080 <87998220+jasonf8080@users.noreply.github.com>
2022-01-22 23:07:59 -0500

    Add files via upload

--- apartments.js
+++ apartments.js
@@ -1,190 +1,103 @@
-//setup local listings innerHTML using js
-const houses = [
-    {
-        img: 'images/house1.png',
-        address: '18 Chestnut St, Yorktown Heights, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house2.png',
-        address: '453 Morningside Ave, New Rochelle, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house3.png',
-        address: '32 Soundview St, Dobbs Ferry, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house4.png',
-        address: '672 Clinton St, Mt. Kisco, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house5.png',
-        address: '843 Clearview St, Yonkers, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house6.png',
-        address: '57 Jackson St, Elmsford, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house7.png',
-        address: '981 Maple Ave, Hawthorne, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house8.png',
-        address: '47 Sterling St, Somers, NY 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house9.png',
-        address: '64 Madison Rd, Peekskill, NY, 12093',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house10.png',
-        address: '843 Columbus Ave, Tarrytown, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house11.png',
-        address: '12 Lincoln St, Briarcliff, NY, 12483',
-        price: '$508,000',
-    },
-
-    {
-        img: 'images/house12.png',
-        address: '129 Ardsley Rd, Ardsley, NY, 12483',
-        price: '$508,000',
-    },
-]
-
 const localListingsGrid = document.querySelector('.local-listings-grid');
 
-let housesHTML = houses.map(function(house){
-    return `    <a href="index4.html" class="l-card">
-    <div class="l-card-img">
-        <img src="${house.img}">
-        <h3>${house.price}</h3>
-        <i class="fas fa-heart"></i>
-    </div>
-
-    <div class="l-card-content">
-
-        <div class="l-card-adress">
-            <p>${house.address}</p>
+fetch('houseInfo.json')
+    .then(res => res.json())
+    .then(data => {
+        const houses = data;
+        let housesContent = houses.map(house => `    
+        <a href="index4.html" class="l-card">
+        <div class="l-card-img">
+            <img src="${house.img}">
+            <h3>${house.price}</h3>
+            <i class="fas fa-heart"></i>
         </div>
-
-        <p class="mobile-price">${house.price}</p>
-
-
-
-
-    <div class="l-card-info">
-        <div class="info">
-            <i class="fas fa-bed"></i>
-            <p>2 bed</p>
-        </div>
-
-        <div class="info">
-            <i class="fas fa-shower"></i>
-            <p>3 baths</p>
-        </div>
-        
-        <div class="info"> 
-            <i class="fas fa-ruler-combined"></i>
-            <p>1280 sq ft</p>
-        </div>
-    </div>
-
+    
+        <div class="l-card-content">
+    
+            <div class="l-card-adress">
+                <p>${house.address}</p>
+            </div>
+    
+            <p class="mobile-price">${house.price}</p>
     
     
-</div>
-
-</a>`;
-
+        <div class="l-card-info">
+            <div class="info">
+                <i class="fas fa-bed"></i>
+                <p>2 bed</p>
+            </div>
     
-});
-
-
-const housesContent = housesHTML.join("");
-localListingsGrid.innerHTML = housesContent;
-
-
-
-//lazy load of houseCards
-const houseCards = document.querySelectorAll('.l-card');
-console.log(houseCards);
-
-let options = {
-    threshold: 0.4,
-};
-
-let houseCardObserver = new IntersectionObserver(function(entries, houseCardObserver){
-    entries.forEach(function(entry){
-        if(entry.isIntersecting){
-            entry.target.style.opacity = '1';
-        } else {
-            entry.target.style.opacity = '0';
-        }
-    })
-}, options)
-
-
-houseCards.forEach(function(houseCard){
-    houseCardObserver.observe(houseCard);
-});
-
-
-//select a house
-houseCards.forEach(function(houseCard){
-    houseCard.addEventListener('click', function(){
-        //img
-        //address
-        //price
-        const houseImg = houseCard.children[0].children[0].src;
-        const houseAddress = houseCard.children[1].children[0].children[0].textContent;
-        const housePrice = houseCard.children[0].children[1].textContent;
-    
-        localStorage.setItem('houseImg', houseImg);
-        localStorage.setItem('houseAddress', houseAddress);
-        localStorage.setItem('housePrice', housePrice);
-
-        let similarHomesArray = [];
-
-        for(let i = 0; i < 4; i++){
-            const randomNumber = Math.floor(Math.random() * houseCards.length);
-            console.log(randomNumber);//generate random number
-
-            similarHomesArray.push(randomNumber);//add random numbers to array
+            <div class="info">
+                <i class="fas fa-shower"></i>
+                <p>3 baths</p>
+            </div>
             
-        }
-
+            <div class="info"> 
+                <i class="fas fa-ruler-combined"></i>
+                <p>1280 sq ft</p>
+            </div>
+        </div>
     
+        
+        
+    </div>
     
-        localStorage.setItem("similarHomes",  JSON.stringify(similarHomesArray));//store array into localStorage as a string
-        
-
-        
-    })
-});
+    </a>`).join('');
+    
+    localListingsGrid.innerHTML = housesContent;
 
 
 
+      //lazy load of houseCards
+        const houseCards = document.querySelectorAll('.l-card');
+        console.log(houseCards);
+
+        let options = {
+            threshold: 0.4,
+        };
+
+        let houseCardObserver = new IntersectionObserver(function(entries, houseCardObserver){
+            entries.forEach(function(entry){
+                if(entry.isIntersecting){
+                    entry.target.style.opacity = '1';
+                } else {
+                    entry.target.style.opacity = '0';
+                }
+            })
+        }, options)
+
+
+        houseCards.forEach(function(houseCard){
+            houseCardObserver.observe(houseCard);
+        });
+
+
+
+
+        //select a house
+        selectHouse(houseCards);
+    
+})
+
+
+
+function selectHouse(houseCards){
+    houseCards.forEach(function(houseCard){
+        houseCard.addEventListener('click', function(){
+            //link to other page, set houseAddress to LS for next page to receive
+            const houseAddress = houseCard.children[1].children[0].children[0].textContent;
+            localStorage.setItem('houseAddress', houseAddress);
+
+        })
+    });
+}
+
+
+
+
+
+
+//make nav fixed
 window.addEventListener('scroll', function(){
     
     const nav =  document.querySelector('.nav');
@@ -196,11 +109,12 @@ window.addEventListener('scroll', function(){
         nav.classList.remove('nav-fixed');
     }
 
-})
+});
 
 
+
+//view menu on mobile
 const menuIcon = document.querySelector('.drop-menu');
-
 
 menuIcon.addEventListener('click', displayMenu);
 
@@ -215,53 +129,11 @@ function displayMenu(e){
     }
 }
 
-let currentHouse = 0;
 
 
 
 
-const searchFilter = document.querySelector('#search-filter');
-
-searchFilter.addEventListener('keyup', function(){
-    const searchFilterInput = searchFilter.value.toUpperCase();
-    const towns = document.querySelectorAll('.towns-list li');
-    
-   
-   
-   
-    for(let i = 0; i < towns.length; i++){
-       
-        let eachTown = towns[i].children[0].textContent;
-        //console.log(eachTown);
-       
-        if(eachTown.toUpperCase().indexOf(searchFilterInput) > -1){
-            towns[i].style.display = 'block';
-            console.log(eachTown.toUpperCase().indexOf(searchFilterInput));//if there is a match.. what index it occurs at
-
-        } else {
-            towns[i].style.display = 'none';
-        }
-    }
 
 
-    //make sure no results show if nothing is typed in
-    if(searchFilterInput === ''){
-        towns.forEach(function(town){
-            town.style.display = 'none';
-        })
-    }
-
-
-    const townLinks = document.querySelectorAll('.towns-list li a');
-    townLinks.forEach(function(link){
-        link.addEventListener('click', function(){
-            const selectedTown = link.textContent;
-            localStorage.setItem('selectedTown', selectedTown);
-
-            
-        })
-    })
-
-})
 
 
--- app4.js
+++ app4.js
@@ -1,76 +1,68 @@
-const houses = [
-    {
-        address: '18 Chestnut St, Yorktown Heights, NY, 12483',
-        imgArray: ['images/house1.png', 'https://kitchensunlimited.net/wp-content/uploads/2019/02/KitchenUnlimited-GlendaRoad-2-SelaviePhoto-A-2.jpg', 'https://www.thespruce.com/thmb/c4b01hm8XRTMPYdQi1lu8Nd-8s0=/2500x1667/filters:fill(auto,1)/brophyinteriorsbedroom4-b262a7064d0c4c14b8ae8f2ae55df805.jpeg', 'https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2020/3/31/0/HUHH2020-Kitchens_Arroyo-Grande-CA_9.jpg.rend.hgtvcom.966.644.suffix/1585683238993.jpeg']
-    },
-
-    {
-        address: '453 Morningside Ave, New Rochelle, NY, 12483',
-        imgArray: ['images/house2.png', 'https://st.hzcdn.com/simgs/pictures/kitchens/small-and-efficient-kitchen-remodel-mosaic-design-build-img~29a1f83a0017f20e_9-0725-1-11fd626.jpg', 'https://furnitureurban.com/wp-content/uploads/2020/04/CMEB6918-scaled.jpg', 'https://kitchenandbathshop.com/wp-content/uploads/2021/02/bathroom-remodel-5000-1024x682.jpg']
-    },
-];
-
-
 const houseImgContainer = document.querySelector('.selected-house');
 const address = document.querySelector('.address');
 const price = document.querySelector('.price');
 
-const houseImg = localStorage.getItem('houseImg');
-const houseAddress = localStorage.getItem('houseAddress');
-const housePrice = localStorage.getItem('housePrice');
+const nextBtn = document.querySelector('.lni.lni-chevron-right');
+const prevBtn = document.querySelector('.lni.lni-chevron-left');
+
+let counter = 0;
+
+fetch('houseInfo.json')
+    .then(res => res.json())
+    .then(data => {
+        const houses = data;
+
+       const houseAddress = localStorage.getItem('houseAddress');
+       console.log(houseAddress);
+
+       for(let i = 0; i < houses.length; i++){
+           if(houseAddress === houses[i].address){
+               const house = houses[i];
+
+               setupHouseImages(house);//ln 31
+               address.textContent = house.address;
+               price.textContent = house.price;
 
 
-address.textContent = houseAddress;
-price.textContent = housePrice;
 
-let imgCounter = 0;
+                //go to next image
+                nextBtn.addEventListener('click', function(){
+                    counter++;
+
+                    if(counter > house.imgArray.length - 1){
+                        counter = 0;
+                    }
+
+                    setupHouseImages(house);
+                });
+
+                //go to previous image
+                prevBtn.addEventListener('click', function(){
+                    counter--;
+
+                    if(counter < 0){
+                        counter = house.imgArray.length - 1;
+                    }
+
+                    setupHouseImages(house);
+                });
+           }
+       }
+
+    })
 
 
 
-for(let i = 0; i < houses.length; i++){
-    if(houseAddress === houses[i].address){
-        var selectedHouse = houses[i];
-        //console.log(selectedHouse);
-        houseImgContainer.src = selectedHouse.imgArray[imgCounter];
-        
-    }
+function setupHouseImages(item){
+    houseImgContainer.src = item.imgArray[counter];
 }
 
-const rightArrow = document.querySelector('.selected-house-container i');
-
-rightArrow.addEventListener('click', function(e){
-    imgCounter++;
-    
-    if(imgCounter > selectedHouse.imgArray.length - 1){
-       imgCounter = 0;
-    }
-    
-    houseImgContainer.src = selectedHouse.imgArray[imgCounter];
-   
-})
 
 
 
-const houseCards = document.querySelectorAll('.l-card');
 
 
-houseCards.forEach(function(houseCard){
-    houseCard.addEventListener('click', function(){
-        //const houseImg = houseCard.children[0].children[0].src;
-        const houseAddress = houseCard.children[1].children[0].children[0].textContent;
-        const housePrice = houseCard.children[0].children[1].textContent;
-    
-        localStorage.setItem('houseImg', houseImg);
-        localStorage.setItem('houseAddress', houseAddress);
-        localStorage.setItem('housePrice', housePrice);
-    })
-})
 
-let similarHomesString = localStorage.getItem('similarHomes');//retrieve similar homes as a string
-let similarHomesArray = JSON.parse(similarHomesString);// turn similarHomesString into an array
 
-similarHomesArray.forEach(function(index){
-    //console.log(index);
 
-})
 
